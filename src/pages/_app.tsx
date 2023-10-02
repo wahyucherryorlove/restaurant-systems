42acc97e04cf43fbb51fe0de53037a9c8e72a1fc
Wahyudi Umar <wahyucherryorlove@gmail.com>
2023-10-02 22:35:40 +0700

    first two

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../../styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+
+import { useResponsive } from "@app/utils/useResponsive";
+
+import { TopNavbar } from "@components/navbar";
+import { TopBarMobile } from "@app/components/navbar/TopBarMobile";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const width = useResponsive();
+  return (
+    <>
+        <Head>
+          {/* <meta charset="UTF-8" /> */}
+          <meta name="description" content="Free Web tutorials" />
+          <meta name="keywords" content="HTML,CSS,XML,JavaScript" />
+          <meta name="author" content="John Doe" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        {width > 767 ? <TopNavbar /> : <TopBarMobile />}
+        <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
