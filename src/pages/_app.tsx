c07b125cf74f7e4e2a0b8c9f9e8f6ba823040208
Wahyudi Umar <wahyucherryorlove@gmail.com>
2023-10-02 22:52:34 +0700

    first three

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,9 +13,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
         <Head>
           {/* <meta charset="UTF-8" /> */}
-          <meta name="description" content="Free Web tutorials" />
-          <meta name="keywords" content="HTML,CSS,XML,JavaScript" />
-          <meta name="author" content="John Doe" />
+          <meta name="description" content="Restoran Systems Terbaru" />
+          <meta name="keywords" content="Restorant Systems" />
+          <meta name="author" content="Wahyudi Umar" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
